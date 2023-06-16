eb57c9999abf1d929d03afe8a11a6b5c3863454b
Joe-Olaya <joel.olaya@hotmail.com>
2023-06-16 16:57:09 -0300

    Update actions.js

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -6,9 +6,9 @@ import axios from "axios";
 const URL = "https://pokeapi.co/api/v2"
 
 export const getPokemons = () => {
-  console.log('pokemons')
-    return async function (dispatch) {
-      const pokemons = (await axios.get(`${URL}/pokemon`));
+  return async function (dispatch) {
+    const pokemons = (await axios.get(`${URL}/pokemon`)).data;
+    console.log(pokemons)
       dispatch({
         type: GET_POKEMONS,
         payload: pokemons,

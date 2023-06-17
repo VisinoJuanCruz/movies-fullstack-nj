06bfae26f07c31b102576870d3d37ea965592c7b
visinodeveloper <visinodeveloper@gmail.com>
2023-06-16 21:04:31 -0300

    Se agregó loading en PokemonCardContainer.jsx
    Se hicieron los estilos para PokemonCard y PokemonCardContainer
    
    Se agregó  bootsrap
    
    Se modificó el title en  index.html
    
    Se modificó PokemonCard

--- client/src/components/PokemonCardContainer/PokemonCardContainer.jsx
+++ client/src/components/PokemonCardContainer/PokemonCardContainer.jsx
@@ -2,6 +2,7 @@ import { getPokemons } from "../../redux/actions";
 import { useEffect, useState } from "react";
 import { PokemonCard } from "../pokemonCard/PokemonCard";
 import { useDispatch, useSelector } from "react-redux";
+import './PokemonCardContainer.css'
 
 export const PokemonCardContainer = () => {
  
@@ -11,22 +12,34 @@ export const PokemonCardContainer = () => {
   useEffect(() => {
   dispatch(getPokemons());
   },[])
+
   const pokemons = useSelector(state => state.pokemons);
-  console.log(pokemons)
+ 
+
+  if(pokemons.length === 0){
+    return <h1>🌀Loading🌀</h1>
+  }
+
+ 
   return (
-    <div className="pokemon">
-      {pokemons.results.map((pokemon) => {
-        return (
-          <div className="pokemon" key={pokemon.id}>
-            <div className="cardPokemon">
-            <PokemonCard 
-              name={pokemon.name}
-            />
-            </div>
-          </div>
-        );
-      })}
+    <div className="pokedex-container" >
+        
+        <h1>Pokedex</h1>
+        <ul className='pokemoncards-container row'>
+        {
+            pokemons.results.map(pokemon => {
+                return(
+                    <li key={pokemon.id} className="col-3">
+                          <PokemonCard pokemonUrl={pokemon.url}/>
+                    </li>
+              
+                )
+            })
+        }
+        </ul>
+
     </div>
-  );
+)
+
 };
 
--- client/src/components/pokemonCard/PokemonCard.jsx
+++ client/src/components/pokemonCard/PokemonCard.jsx
@@ -1,13 +1,61 @@
-export const PokemonCard = ({name}) => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './PokemonCard.css'
+
+export const PokemonCard = ({pokemonUrl}) =>  {
+
+    const [pokemon, setPokemon] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+      }
+
+    useEffect(() => {
+        axios.get(`${pokemonUrl}`)
+        .then(res => { const newPokemon = res.data
+        setPokemon(newPokemon)
+    setLoading(false)})
+    }, [])
+    
+    console.log(pokemon)
+
+    if(loading) return <h1>Loading...</h1>
+
+    const className = `pokemon-card-container ${pokemon.types[0].type.name}`
 
     return (
-        <div className="card">
-            <img src="" alt="" />
-            <div className="card-body">
-                <h2>{name}</h2>
+        <div className={className}>
+            <div className="pokemon-card">
+                <div className="background">
+                    <img className="pokemon-image img-fluid" src={pokemon.sprites.other.dream_world.front_default} alt="" /> 
+                </div>
+
+
+              <div className="content">
+                <h1 className="pokemon-name">
+                    { capitalize(pokemon.name) }
+                </h1>
+              
+                <span className="pokemon-type">
+                    {pokemon.types[0].type.name}
+                </span>
+              
+                <div className="pokemon-stats row">
+                        {
+                        pokemon.stats.map(stat => {
+                            return(
+                                <p className="stat col-6 " key={stat.stat.name}>{capitalize(stat.stat.name)}: {stat.base_stat}</p>
+                            )})
+                        }
+                </div>
+
+              </div>
             </div>
+            
+            
+            
         </div>
     )
-
 
 }
